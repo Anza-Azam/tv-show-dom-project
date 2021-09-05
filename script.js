79b681926b2ed4f04224890d632432e04fd7b66c
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-05 20:39:26 +0100

    feat:styling updated

--- script.js
+++ script.js
@@ -1,61 +1,42 @@
-//You can edit ALL of the code here
+// You can edit ALL of the code here
 function setup() {
-	const allEpisodes = getAllEpisodes();
-	makePageForEpisodes(allEpisodes);
-  
-	let rootElem = document.createElement('div');
-  rootElem.classList.add('root')
-	let header = document.createElement('header');
-  document.body.appendChild(header);
-  document.body.appendChild(rootElem);
-	header.classList.add('header');
+    const allEpisodes = getAllEpisodes();
+    makePageForEpisodes(allEpisodes);
 
-	
-    let heading=document.createElement('h1')
- 
-	heading.innerText = 'TV Shows';
+    let rootElem = document.createElement('div');
+    rootElem.classList.add('root');
+    let header = document.createElement('header');
+    document.body.appendChild(header);
+    document.body.appendChild(rootElem);
+    header.classList.add('header');
 
-  header.append(heading);
-  let main= document.createElement('main')
-  main.classList.add('main');
-  rootElem.appendChild(main)
-  allEpisodes.forEach(shows=>{ const show=document.createElement('div');
-  show.classList.add('showStyles')
-  const showHeading=document.createElement('h2')
-  const imgDiv=document.createElement('div')
-  const showImage=document.createElement('img')
-  const showDescription=document.createElement('p')
-  showHeading.classList.add('showItem')
-  showImage.classList.add('showItem')
-  showDescription.classList.add('showItem')
-  showHeading.innerText= `${shows.name}-${shows.season.toString().padStart(3,"S0")}${shows.number.toString().padStart(3,"E0")}`
-  showImage.src = shows.image.medium
-  imgDiv.appendChild(showImage)
-  showDescription.innerText=shows.summary.substring(3,shows.summary.length-4)
-  show.append(showHeading,imgDiv,showDescription)
-  main.append(show)
+    let heading = document.createElement('h1');
 
-  main.appendChild(show)
+    heading.innerText = 'TV Shows';
 
+    header.append(heading);
+    let main = document.createElement('main');
+    main.classList.add('main');
+    rootElem.appendChild(main);
+    allEpisodes.forEach((shows) => { const show = document.createElement('div'); show.classList.add('showStyles'); const showHeading = document.createElement('h2'); const imgDiv = document.createElement('div'); const showImage = document.createElement('img'); const showDescription = document.createElement('p'); showHeading.classList.add('showItem'); showImage.classList.add('showItem'); showDescription.classList.add('showItem'); showHeading.innerText = `${shows.name}-${shows.season .toString() .padStart(3, 'S0')}${shows.number.toString().padStart(3, 'E0')}`; showImage.src = shows.image.medium; imgDiv.appendChild(showImage); showDescription.innerText = shows.summary.substring(3, shows.summary.length - 4); show.append(showHeading, imgDiv, showDescription); main.append(show); main.appendChild(show); });
 
-});
-  
-let footer= document.createElement('footer')
-footer.classList.add('footer')
-let site= document.createElement('p')
-site.innerText="@copyright TVMaze.com"
-document.body.appendChild(footer)
-footer.appendChild(site)
+    let footer = document.createElement('footer');
+    footer.classList.add('footer');
+    let site = document.createElement('p');
+    site.innerText = '@copyright TVMaze.com';
+    document.body.appendChild(footer);
+    footer.appendChild(site);
 }
 
 function makePageForEpisodes(episodeList) {
-	// const rootElem = document.getElementById("root");
-	// rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+    ;
+    // const rootElem = document.getElementById("root");
+    // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
 
-///creating header and grid
-//let root = document.getElementById("root")
+// /creating header and grid
+// let root = document.getElementById("root")
 
-//const main= document.createElement('div')
+// const main= document.createElement('div')

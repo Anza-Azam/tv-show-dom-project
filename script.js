0596256fd18d2510478e670f54e16b8748501bb0
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-05 13:32:43 +0100

    feat:main added

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+	const allEpisodes = getAllEpisodes();
+	makePageForEpisodes(allEpisodes);
+	let rootElem = document.getElementById('root');
+	let header = document.createElement('header');
+	header.classList.add('header');
+	header.innerText = 'TV Shows';
+	rootElem.appendChild(header);
+  let main= document.createElement('main')
+  main.classList.add('main');
+  rootElem.appendChild(main)
+  allEpisodes.forEach(shows=>{ const show=document.createElement('div');
+  show.classList.add('showStyles')
+  const showHeading=document.createElement('h2')
+  const showImage=document.createElement('img')
+  const showDescription=document.createElement('p')
+  showHeading.classList.add('showItem')
+  showImage.classList.add('showItem')
+  showDescription.classList.add('showItem')
+  showHeading.innerText= `${shows.name}-${shows.season}${shows.number}`
+  showImage.src = shows.image.medium
+  showDescription.innerText=shows.summary.substring(3,shows.summary.length-4)
+  show.append(showHeading,showImage,showDescription)
+  main.append(show)
+
+  main.appendChild(show)
+
+
+});
+  
+
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+	// const rootElem = document.getElementById("root");
+	// rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
+
+///creating header and grid
+//let root = document.getElementById("root")
+
+//const main= document.createElement('div')

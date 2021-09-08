db65afbbf3878122ee6bdb9639c843940043942d
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-08 10:48:06 +0100

    feat: level 200 done

--- script.js
+++ script.js
@@ -56,39 +56,20 @@ function setup() {
     footer.appendChild(link)
     let arr=[]
      allEpisodes.forEach(displayShows);
-   search.addEventListener('input',()=>{
-               let timeout;
-clearTimeout(timeout);
-    timeout = setTimeout(() => {
-    // removeAll();
    
+search.addEventListener('keyup',function(e)
+{
 
-filtered = allEpisodes.filter((episode) =>
-    {
-        if (episode.summary.toLowerCase().includes(search.value)||episode.name.toLowerCase().includes(search.value)){
+     const lists = document.querySelector('.main')
 
-            return episode;
-        }
-           });
+const term=e.target.value.toLowerCase();
+const itemsfrom = lists.getElementsByClassName('showStyles')
+//console.log(itemsfrom)
+Array.from(itemsfrom).forEach(elem=>{ if(!elem.firstElementChild.textContent.toLowerCase().includes(term)&&(!elem.lastElementChild.textContent.toLowerCase().includes(term))) 
+    {elem.style.display='none'}
+}
 
-arr=filtered.slice();
- //console.log(arr.length)
-
-             displayNumbers.innerText=`Displaying ${arr.length}/${allEpisodes.length} Episodes`;
-       
-    }, 1000);
-
-//console.log(arr.length)
-arr.forEach(shows =>{  removeAll();
-     const show = document.createElement('div');
-console.log(shows)
-     show.classList.add('showStyles'); const showHeading = document.createElement('h2'); const imgDiv = document.createElement('div'); const showImage = document.createElement('img'); const showDescription = document.createElement('p'); showHeading.classList.add('showItem'); showImage.classList.add('showItem'); showDescription.classList.add('showItem'); showHeading.innerText = `${shows.name}-${shows.season .toString() .padStart(3, 'S0')}${shows.number.toString().padStart(3, 'E0')}`; showImage.src = shows.image.medium; imgDiv.appendChild(showImage); showDescription.innerText = shows.summary.substring(3, shows.summary.length - 4); show.append(showHeading, imgDiv, showDescription); mains.appendChild(show);removeAll()
-    
-   console.log(showDescription) 
-});
-
- rootElem.appendChild(mains);
-
+)
 })
 
 

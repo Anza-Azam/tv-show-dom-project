512b94b2c14aa59384858642333057ef03b2f947
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-18 07:43:30 +0100

    tv-dom with fetch modified

--- episodecard.js
+++ episodecard.js
@@ -1,30 +1,23 @@
-//import getAllEpisodes from "./script.js";
+//display episodes on page
 
-//let allEpisodes = [];
-    
-        
-    
+export default function createEpisodeCard (allEpisodes) {
+  const episodeList = document.createElement('ul') // Episode list
 
-export default function createEpisodeCard(allEpisodes) {
-
-  //  allEpisodes = await getAllEpisodes();
-  const episodeList = document.createElement('ul'); // Episode list
-  // console.log(allEpisodes.length);
-  episodeList.classList.add('main'); //main class attached to list
-  allEpisodes.forEach((episode) => {
+  episodeList.classList.add('main') //main class attached to list
+  allEpisodes.forEach(episode => {
     //for each episode a li card is created
-    const lI = document.createElement('li');
-    const episodeVersionH2 = document.createElement('h2'); // episode heading
-    const season = episode.season, number = episode.number;
-    
+    const lI = document.createElement('li')
+    const episodeVersionH2 = document.createElement('h2') // episode heading
+    const season = episode.season,
+      number = episode.number
 
-      const formattedSeason = ('' + season).padStart(2, '0'); //type coersion
-      const formattedNumber = ('' + number).padStart(2, '0'); //type coersion
-      const episodeVersion = `S${formattedSeason}E${formattedNumber}`; //full version to present
+    const formattedSeason = ('' + season).padStart(2, '0') //type coersion
+    const formattedNumber = ('' + number).padStart(2, '0') //type coersion
+    const episodeVersion = `S${formattedSeason}E${formattedNumber}` //full version to present
 
-      episodeVersionH2.innerText = name + ' - ' + episodeVersion;
+    episodeVersionH2.innerText = name + ' - ' + episodeVersion
     //let episodeVersionH2;
-    lI.className = 'episode-card';
+    lI.className = 'episode-card'
     if (episode.image !== null) {
       if (episode.summary !== null) {
         const name = episode.name,
@@ -32,52 +25,44 @@ export default function createEpisodeCard(allEpisodes) {
           season = episode.season,
           number = episode.number,
           summary = episode.summary.substring(3, episode.summary.length - 4), // removed summary tags that each episode had in summary
-          image = episode.image.medium;
+          image = episode.image.medium
 
-     
-      
+        const formattedSeason = ('' + season).padStart(2, '0') //type coersion
+        const formattedNumber = ('' + number).padStart(2, '0') //type coersion
+        const episodeVersion = `S${formattedSeason}E${formattedNumber}` //full version to present
 
-        const formattedSeason = ('' + season).padStart(2, '0'); //type coersion
-        const formattedNumber = ('' + number).padStart(2, '0'); //type coersion
-        const episodeVersion = `S${formattedSeason}E${formattedNumber}`; //full version to present
+        const episodeVersionH2 = document.createElement('h2') // episode heading
+        episodeVersionH2.innerText = name + ' - ' + episodeVersion
 
-        const episodeVersionH2 = document.createElement('h2'); // episode heading
-        episodeVersionH2.innerText = name + ' - ' + episodeVersion;
+        const imageContainer = document.createElement('div') // episode image
+        imageContainer.classList.add('image-container')
+        const episodeImg = document.createElement('img')
+        imageContainer.appendChild(episodeImg)
+        episodeImg.src = image // image source is link to medium sized image
+        episodeImg.alt = 'show episode'
 
-        const imageContainer = document.createElement('div'); // episode image
-        imageContainer.classList.add('image-container');
-        const episodeImg = document.createElement('img');
-        imageContainer.appendChild(episodeImg);
-        episodeImg.src = image;                        // image source is link to medium sized image
-        episodeImg.alt = 'show episode';
+        const episodeSummary = document.createElement('p') //episode summary
+        episodeSummary.classList.add('summary')
+        episodeSummary.innerText = summary
+       
 
-        const episodeSummary = document.createElement('p');  //episode summary
-        episodeSummary.classList.add('summary');
-        episodeSummary.innerText = summary;
-        const search= document.getElementById('searchBox')
-        search.removeAttribute('disabled')
+        lI.appendChild(episodeVersionH2)
+        lI.appendChild(imageContainer)
+        lI.appendChild(episodeSummary)
 
-        lI.appendChild(episodeVersionH2);
-        lI.appendChild(imageContainer);
-        lI.appendChild(episodeSummary);
-
-        episodeList.appendChild(lI); // appended li to ul element
+        episodeList.appendChild(lI) // appended li to ul element
       }
-    }
-
-
-    else {
-      lI.innerText = 'Sorry, episode is not available';
-       episodeVersionH2.innerText = episode.name + ' - ' + episodeVersion;
-      lI.appendChild(episodeVersionH2); episodeList.appendChild(lI);
-      
+    } else {
+      lI.innerText = 'Sorry, episode is not available'
+      episodeVersionH2.innerText = episode.name + ' - ' + episodeVersion
+      lI.appendChild(episodeVersionH2)
+      episodeList.appendChild(lI)
     }
   })
 
-    const rootEl = document.getElementById('root');
+  const rootEl = document.getElementById('root')
 
-    // targeting root element in the DOM
+  // targeting root element in the DOM
 
-  rootEl.appendChild(episodeList);
-  
+  rootEl.appendChild(episodeList)
 }
--- script.js
+++ script.js
@@ -1,287 +1,237 @@
 //import episode card to render the data
-import createEpisodeCard from "./episodecard.js";
-import getAllShows from "./shows.js";
+import createEpisodeCard from './episodecard.js'
+import getAllShows from './shows.js'
 
-
-let allShowEpisodes = [];
-let allEpisodes = [];
+let allShowEpisodes = []
+let allEpisodes = []
 let allAvailableShows = []
-let userSelectedShow = "";
-
-async function getAllEpisodes() {
-    const response = await fetch("https://api.tvmaze.com/shows/82/episodes")
-    const data = await response.json()
-    return data
+let userSelectedShow = ''
+//first show for local storage
+async function getAllEpisodes () {
+  const response = await fetch('https://api.tvmaze.com/shows/82/episodes')
+  const data = await response.json()
+  return data
+}
+if (!localStorage.getItem('episodes')) {
+  getAllEpisodes().then(data =>
+    localStorage.setItem('episodes', JSON.stringify(data))
+  )
 }
 
-if(!localStorage.getItem("episodes")) {
-    getAllEpisodes()
-        .then(data => 
-            localStorage.setItem("episodes", JSON.stringify(data))
-        )
-}
-
-const episodesString = localStorage.getItem("episodes")
+const episodesString = localStorage.getItem('episodes')
 const episodesArray = JSON.parse(episodesString)
-//console.log(episodesArray);
-
-// async function getAllEpisodes() {
-//     let e= document.getElementById('shows');
-//    // let nme= e.options[e.selectedIndex].text;
-    
-//     allAvailableShows = getAllShows();
-//     console.log(userSelectedShow)
-//     //let element= allAvailableShows.find(ele=>ele.name===nme)
-//     //id = element.id;
-//     let url = `https://api.tvmaze.com/shows/82/episodes`;
-//     let res = fetch(url).then(res => res.json()).catch(error => console.log(error))
-//     return res;
-  
-
-        
-// }
-    
 
 //Setup
-async function setup() {
-   
-    //allShowEpisodes = await getAllEpisodes();
-    allEpisodes = episodesArray;
-    allAvailableShows =getAllShows();
-    ////allEpisodes = allShowEpisodes.map(episode => episode)
-    //console.log(allEpisodes)
-    //console.log(allEpisodes.length);
-     makePageForEpisodes(); // initial page for all episodes
-    createEpisodeCard(allEpisodes); //single episode card for available / matched episode
-    createOptions(allEpisodes);
-    showOptions(allAvailableShows); //available episodes in the selection
-    
-    
+async function setup () {
+  
+  allEpisodes = episodesArray //all episodes
+  allAvailableShows = getAllShows() //all shows
+  makePageForEpisodes() // initial page for all episodes
+  createEpisodeCard(allEpisodes) //single episode card for available / matched episode
+  createOptions(allEpisodes) //load episodes in select
+  showOptions(allAvailableShows) //load shows in select
 }
 
+// header body and footer styling and data presentation
+function makePageForEpisodes () {
+  const rootElem = document.getElementById('root') //root div with ul
+  const header = document.createElement('header')
+  document.body.prepend(header)
+  header.classList.add('header')
+  const heading = document.createElement('h1')
+  heading.innerText = 'TV Shows'
+  header.appendChild(heading)
+  const searchContainer = document.createElement('div') //search input text and display
+  const displayNumbers = document.createElement('p')
+  displayNumbers.id = 'display'
+  const search = document.createElement('input')
+  const searchLabel = document.createElement('label')
+  searchLabel.classList.add('label')
+  searchLabel.innerText = 'Search Episode'
+  search.type = 'text'
+  search.placeholder = 'Enter key word'
+  search.id = 'searchBox'
+  searchContainer.classList.add('inputs')
+  searchContainer.append(searchLabel, search, displayNumbers)
+  document.body.insertBefore(searchContainer, rootElem)
+  const selectLabel = document.createElement('label')
+  selectLabel.innerText = 'Select Episode'
+  selectLabel.classList.add('label')
+  const selectLabelOptions = document.createElement('label')
+  selectLabelOptions.innerText = 'Select Show'
+  selectLabelOptions.classList.add('label')
+  const select = document.createElement('select')
+  //select display for options of episodes available
+  const showNames = document.createElement('select')
+  showNames.classList.add('select')
+  showNames.id = 'shows'
+  select.classList.add('select')
+  const selectContainer = document.createElement('div')
+  const button = document.createElement('button')
+  button.style.display = 'none'
+  button.classList.add('button')
+  button.innerText = 'Show all Episodes'
+  selectContainer.append(selectLabel, select, selectLabelOptions, showNames, button)
 
-
-
-// header body and footer styling and data presentation 
- function makePageForEpisodes() {
-    const rootElem = document.getElementById('root'); //root div with ul
-
-    const header = document.createElement('header');
-    document.body.prepend(header);
-    header.classList.add('header');
-
-    const heading = document.createElement('h1');
-
-    heading.innerText = 'TV Shows';
-    header.appendChild(heading);
-
-    const searchContainer = document.createElement('div');       //search input text and display
-    const displayNumbers = document.createElement('p');
-    displayNumbers.id = 'display';
-    const search = document.createElement('input');
-    const searchLabel = document.createElement('label');
-    searchLabel.classList.add('label')
-    searchLabel.innerText = "Search Episode";
-    search.type = 'text';
-    search.placeholder = 'Enter key word';
-     search.id = 'searchBox'
-     //const searchbox = document.getElementById('searchBox')
-      search.removeAttribute('disabled')
-      
-    searchContainer.classList.add('inputs');
-    searchContainer.append(searchLabel, search, displayNumbers);
-    document.body.insertBefore(searchContainer, rootElem);
-    const selectLabel = document.createElement('label');
-    selectLabel.innerText = 'Select Episode'
-     selectLabel.classList.add('label')
-     const selectLabel2 = document.createElement('label');
-    selectLabel2.innerText = 'Select Show'
-    selectLabel2.classList.add('label')
-     const select = document.createElement('select');
-               //select display for options of episodes available
-     const showNames = document.createElement('select');
-     showNames.classList.add('select');
-     showNames.id='shows'
-     select.classList.add('select');
-    const selectContainer = document.createElement('div');
-    const button = document.createElement('button');
-    button.style.display = 'none';
-    button.classList.add('button');
-    button.innerText = 'Show all Episodes';
-    selectContainer.append(selectLabel, select,selectLabel2, showNames,  button);
-
-    document.body.insertBefore(selectContainer, searchContainer); //select and search appended to the body 
-    selectContainer.classList.add('selection');
-    const footer = document.createElement('footer');
-    footer.classList.add('footer');
-    const site = document.createElement('p');
-    site.innerText = '@copyright';
-    const link = document.createElement('a');
-    link.id = 'linkTag';
-    link.href = 'https://www.tvmaze.com/api#licensing';
-    link.target = '_blank';
-    link.innerText = 'TVMaze.com';
-
-    document.body.appendChild(footer);
-    footer.appendChild(site);
-
-    footer.appendChild(link);
-    //button event to display all episodes
-     button.addEventListener('click', async () => {
-         allShowEpisodes = await getAllEpisodes2();
-       //  allEpisodes = episodesArray;
+  document.body.insertBefore(selectContainer, searchContainer) //select and search appended to the body
+  selectContainer.classList.add('selection')
+  const footer = document.createElement('footer')
+  footer.classList.add('footer')
+  const site = document.createElement('p')
+  site.innerText = '@copyright'
+  const link = document.createElement('a')
+  link.id = 'linkTag'
+  link.href = 'https://www.tvmaze.com/api#licensing'
+  link.target = '_blank'
+  link.innerText = 'TVMaze.com'
+  //display footer
+  document.body.appendChild(footer)
+  footer.appendChild(site)
+  footer.appendChild(link)
+  //button event to display all episodes
+  button.addEventListener('click', async () => {
+    allShowEpisodes = await getAllEpisodesFetched()
     allEpisodes = allShowEpisodes.map(episode => episode)
-    console.log(allEpisodes.length)
-    const screen = document.getElementById('root');
-    const list = document.querySelector('.main');
-    screen.removeChild(list);
-    //createEpisodeCard(selected);
-    const options = document.querySelector('.select');
-    removeAllChildNodes(options);
-
-    createEpisodeCard(allEpisodes); //single episode card for available / matched episode
-    createOptions(allEpisodes);
+   
+    const screen = document.getElementById('root')
+    const list = document.querySelector('.main')
+    screen.removeChild(list)
     
-    });
-    searchContainer.addEventListener('input', searchList);//event listener search
-     select.addEventListener('click', selectList);//event listener select 
-     showNames.addEventListener('click', selectShows);
-     
+    const options = document.querySelector('.select')
+    removeAllChildNodes(options)
+
+    createEpisodeCard(allEpisodes) //single episode card for available / matched episode
+    createOptions(allEpisodes)
+  })
+  searchContainer.addEventListener('input', searchList) //event listener search
+  select.addEventListener('click', selectList) //event listener select
+  showNames.addEventListener('click', selectShows)
 }
 
 // populate Episode options when episode are searched or all episodes if no search made
-function createOptions(episodeList) {
-    const selectOptions = document.querySelector('select');
- const search = document.getElementById('searchBox');
-    search.innerText=''
-    episodeList.forEach((episode) => {
-        const option = document.createElement('option');
-        option.value = `${episode.season.toString().padStart(3, 'S0')}${episode.number
-            .toString()
-            .padStart(3, 'E0')} - ${episode.name}`;
-        option.innerText = `${episode.season.toString().padStart(3, 'S0')}${episode.number
-            .toString()
-            .padStart(3, 'E0')} - ${episode.name}`;
-        selectOptions.appendChild(option);
-    });
+function createOptions (episodeList) {
+  const selectOptions = document.querySelector('select')
+  const search = document.getElementById('searchBox')
+  search.innerText = ''
+  episodeList.forEach(episode => {
+    const option = document.createElement('option')
+    option.value = `${episode.season
+      .toString()
+      .padStart(3, 'S0')}${episode.number.toString().padStart(3, 'E0')} - ${
+      episode.name
+    }`
+    option.innerText = `${episode.season
+      .toString()
+      .padStart(3, 'S0')}${episode.number.toString().padStart(3, 'E0')} - ${
+      episode.name
+    }`
+    selectOptions.appendChild(option)
+  })
 }
 
-function showOptions(episodeList) {
-    const selectOptions = document.getElementById('shows');
-    episodeList = episodeList.sort((a, b)=> {
-    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
-    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
-    return 0;
-  }
-);
-    episodeList.forEach((episode) => {
-        const option = document.createElement('option');
-        
-            option.value = `${episode.name}`;
-           // console.log(episode.name);
-            option.innerText = `${episode.name}`;
-            selectOptions.appendChild(option);
-        
-    });
+function showOptions (episodeList) {
+  const selectOptions = document.getElementById('shows')
+  episodeList = episodeList.sort((a, b) => {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
+    return 0
+  })
+  episodeList.forEach(episode => {
+    const option = document.createElement('option')
+
+    option.value = `${episode.name}`
+   
+    option.innerText = `${episode.name}`
+    selectOptions.appendChild(option)
+  })
 }
 //search event listener's call back function
-  async function searchList(e) {
-    const screen = document.getElementById('root');
-    const list = document.querySelector('.main');
-    screen.removeChild(list);
-    const displayNumbers = document.getElementById('display');
-    const options = document.querySelector('.select');
-    removeAllChildNodes(options);
+async function searchList (e) {
+  const screen = document.getElementById('root')
+  const list = document.querySelector('.main')
+  screen.removeChild(list)
+  const displayNumbers = document.getElementById('display')
+  const options = document.querySelector('.select')
+  removeAllChildNodes(options)
 
-   // allEpisodes = await getAllEpisodes();
-      
-    const userInput = e.target.value.toLowerCase();
-      const allMatchedEpisodes = allEpisodes.filter((episode) => {
-          if (episode.image !== null && episode.summary !== null) {
-              if (episode.name.toLowerCase().includes(userInput) || episode.summary.toLowerCase().includes(userInput))
-                  return episode;
-          } 
-    });
-    displayNumbers.innerText = `displaying ${allMatchedEpisodes.length} / ${allEpisodes.length}`;
-    createEpisodeCard(allMatchedEpisodes);
-    createOptions(allMatchedEpisodes);
+
+  const userInput = e.target.value.toLowerCase()
+  const allMatchedEpisodes = allEpisodes.filter(episode => {
+    if (episode.image !== null && episode.summary !== null) {
+      if (
+        episode.name.toLowerCase().includes(userInput) ||
+        episode.summary.toLowerCase().includes(userInput)
+      )
+        return episode
+    }
+  })
+  displayNumbers.innerText = `displaying ${allMatchedEpisodes.length} / ${allEpisodes.length}`
+  createEpisodeCard(allMatchedEpisodes)
+  createOptions(allMatchedEpisodes)
 }
 //select event listener's call back function
-async function selectList(e) {
-    const search = document.getElementById('searchBox');
-    search.innerText=''
-    const button = document.querySelector('.button');
-    button.style.display = 'flex';
-   // allEpisodes =await getAllEpisodes();
-    const selected =allEpisodes.filter((episode) => {
-        if (
-            e.target.value ===
-            `${episode.season.toString().padStart(3, 'S0')}${episode.number
-                .toString()
-                .padStart(3, 'E0')} - ${episode.name}`
-        ) {
-            return episode;
-        }
-    });
-
-    
-    const screen = document.getElementById('root');
-    const list = document.querySelector('.main');
-    screen.removeChild(list);
-    createEpisodeCard(selected);
-}
-async function getAllEpisodes2() {
-   // let e= document.getElementById('shows');
-   // let nme= e.options[e.selectedIndex].text;
-    let id;
-    
-    allAvailableShows = getAllShows();
-    console.log(userSelectedShow)
-    let element = allAvailableShows.find(ele => ele.name === userSelectedShow)
-    if (element !== undefined) {
-        id = element.id;
-    }
-    else (id=82)
-    console.log(id)
-    let url = `https://api.tvmaze.com/shows/${id}/episodes`;
-    let res = fetch(url).then(res => res.json()).catch(error => console.log("connection Failed", error))
-    return res;
-  
-
-        
-}
-async function selectShows(e) {
-
-    userSelectedShow = e.target.value;
-    const displayNumbers=document.getElementById('display')
-    displayNumbers.innerText = '';
-    const inputBox=document.getElementById('searchBox')
-    inputBox.innerText = '';
-       
-    allShowEpisodes = await getAllEpisodes2();
-   // allEpisodes = episodesArray;
-    allEpisodes = allShowEpisodes.map(episode => episode)
-    console.log(allEpisodes.length)
-    const screen = document.getElementById('root');
-    const list = document.querySelector('.main');
-    screen.removeChild(list);
-    //createEpisodeCard(selected);
-    const options = document.querySelector('.select');
-    removeAllChildNodes(options);
-    //const showOption= document.getElementById('shows');
-   // removeAllChildNodes(showOption);
-    createEpisodeCard(allEpisodes); //single episode card for available / matched episode
-    createOptions(allEpisodes);
-   // showOptions(allAvailableShows); //available episodes in the selection
-    
-    
-}
-    //console.log(userSelectedShow)
+async function selectList (e) {
+  const search = document.getElementById('searchBox')
+  search.value = ''
+  const button = document.querySelector('.button')
+  button.style.display = 'flex'
  
-//remove options dynamically
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+  const selected = allEpisodes.filter(episode => {
+    if (
+      e.target.value ===
+      `${episode.season
+        .toString()
+        .padStart(3, 'S0')}${episode.number.toString().padStart(3, 'E0')} - ${
+        episode.name
+      }`
+    ) {
+      return episode
     }
+  })
+
+  const screen = document.getElementById('root')
+  const list = document.querySelector('.main')
+  screen.removeChild(list)
+  createEpisodeCard(selected)
 }
-window.onload = setup;          // on window load setup call
+async function getAllEpisodesFetched () {
+  
+  let id
+  allAvailableShows = getAllShows()  
+  let element = allAvailableShows.find(ele => ele.name === userSelectedShow)
+  if (element !== undefined) {
+    id = element.id
+  } else id = 82  
+  let url = `https://api.tvmaze.com/shows/${id}/episodes`
+  let res = fetch(url)
+    .then(res => res.json())
+    .catch(error => console.log('connection Failed', error))
+  return res
+}
+//new show select
+async function selectShows (e) {
+  userSelectedShow = e.target.value
+  const displayNumbers = document.getElementById('display')
+  displayNumbers.innerText = ''
+  const inputBox = document.getElementById('searchBox')
+  inputBox.value = ''
+  allShowEpisodes = await getAllEpisodesFetched()
+  allEpisodes = allShowEpisodes.map(episode => episode)
+  const screen = document.getElementById('root')
+  const list = document.querySelector('.main')
+  screen.removeChild(list)  
+  const options = document.querySelector('.select')
+  removeAllChildNodes(options)  
+  createEpisodeCard(allEpisodes) //single episode card for available / matched episode
+  createOptions(allEpisodes)
+  
+}
+
+
+//remove options dynamically
+function removeAllChildNodes (parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+}
+window.onload = setup // on window load setup call
